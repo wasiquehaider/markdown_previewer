b19268a7a3fcb1187557fc4177ec972c9abbe0c4
wasiquehaider <wasiquehaider02@gmail.com>
2018-10-04 12:32:15 -0700

    MarkDown Previewer Using React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import FormGroup from "react-bootstrap/lib/FormGroup";
+import ControlLabel from "react-bootstrap/lib/ControlLabel";
+import FormControl from "react-bootstrap/lib/FormControl";
+import Grid from "react-bootstrap/lib/Grid";
+import Row from "react-bootstrap/lib/Row";
+import Col from "react-bootstrap/lib/Col";
+
+let marked = require("marked");
 
 class App extends Component {
+  state = {
+    markdown: ""
+  };
+
+  updateMarkDown = function(markdown) {
+    this.setState({ markdown });
+  };
   render() {
+    let { markdown } = this.state;
+
+    console.log(markdown);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App container">
+        <h1 className="text-center">MarkDown Previewer Using React</h1>
+        <Grid>
+          <Row>
+            <Col md={6} mdPush={6}>
+              <FormGroup controlId="FormControlsTextArea">
+                <ControlLabel>MarkDown Input</ControlLabel>
+                <FormControl
+                  componentClass="textarea"
+                  rows={20}
+                  placeholder="Enter Markdown e.g <b>Hello</b>"
+                  value={markdown}
+                  onChange={e => this.updateMarkDown(e.target.value)}
+                />
+              </FormGroup>
+            </Col>
+            <Col md={6} mdPush={6}>
+              <p>Markdown Output</p>
+              <div
+                className="markdownOutput"
+                dangerouslySetInnerHTML={{ __html: marked(markdown) }}
+              />
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
